67568381148bc0e9d3a61dc147a217b11e880a85
katorabian <katorabian@gmail.com>
2025-06-17 19:36:39 +0700

    feat(structure): move according to clean architecture

--- app/src/main/java/com/katorabian/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/katorabian/weatherapp/presentation/MainActivity.kt
@@ -1,17 +1,10 @@
-package com.katorabian.weatherapp
+package com.katorabian.weatherapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.katorabian.weatherapp.ui.theme.WeatherAppTheme
+import com.katorabian.weatherapp.presentation.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/katorabian/weatherapp/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/katorabian/weatherapp/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.katorabian.weatherapp.ui.theme
+package com.katorabian.weatherapp.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
