2a6a9e3a6e8ad0ec72b80fbaf15dc8e505074d07
katorabian <katorabian@gmail.com>
2025-06-23 12:53:21 +0700

    feat(structure): finish implement FavoriteRepository

--- app/src/main/java/com/katorabian/weatherapp/data/repository/FavoriteRepositoryImpl.kt
+++ app/src/main/java/com/katorabian/weatherapp/data/repository/FavoriteRepositoryImpl.kt
@@ -1,22 +1,27 @@
 package com.katorabian.weatherapp.data.repository
 
+import com.katorabian.weatherapp.data.local.db.FavoriteCitiesDao
+import com.katorabian.weatherapp.data.mapper.toDbModel
+import com.katorabian.weatherapp.data.mapper.toEntities
 import com.katorabian.weatherapp.domain.entity.City
 import com.katorabian.weatherapp.domain.repository.FavoriteRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class FavoriteRepositoryImpl : FavoriteRepository {
-    override val favoriteCities: Flow<List<City>>
-        get() = TODO("Not yet implemented")
+class FavoriteRepositoryImpl @Inject constructor(
+    private val favoriteCitiesDao: FavoriteCitiesDao
+) : FavoriteRepository {
+    override val favoriteCities: Flow<List<City>> = favoriteCitiesDao
+        .getFavoriteCities()
+        .map { it.toEntities() }
 
-    override fun observeIsFavorite(cityId: Int): Flow<Boolean> {
-        TODO("Not yet implemented")
-    }
+    override fun observeIsFavorite(cityId: Int): Flow<Boolean> =
+        favoriteCitiesDao.observeIsFavorite(cityId)
 
-    override suspend fun addToFavorite(city: City) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun addToFavorite(city: City) =
+        favoriteCitiesDao.addToFavorite(city.toDbModel())
 
-    override suspend fun removeFromFavorite(cityId: Int) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun removeFromFavorite(cityId: Int) =
+        favoriteCitiesDao.removeFromFavorite(cityId)
 }
--- app/src/main/java/com/katorabian/weatherapp/domain/repository/FavoriteRepository.kt
+++ app/src/main/java/com/katorabian/weatherapp/domain/repository/FavoriteRepository.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface FavoriteRepository {
 
-    val favoriteCities: Flow<City>
+    val favoriteCities: Flow<List<City>>
 
     fun observeIsFavorite(cityId: Int): Flow<Boolean>
 
