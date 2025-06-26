35b1965038733b02fc1b59cab10727deca57fbe9
katorabian <katorabian@gmail.com>
2025-06-27 00:41:21 +0700

    feat(component): implement Root- ComponentContext

--- app/src/main/java/com/katorabian/weatherapp/presentation/screen/root/DefaultRootComponent.kt
+++ app/src/main/java/com/katorabian/weatherapp/presentation/screen/root/DefaultRootComponent.kt
@@ -1,7 +1,70 @@
 package com.katorabian.weatherapp.presentation.screen.root
 
+import android.os.Parcelable
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.value.Value
+import com.katorabian.weatherapp.domain.entity.City
+import com.katorabian.weatherapp.presentation.screen.details.DefaultDetailsComponent
+import com.katorabian.weatherapp.presentation.screen.favorite.DefaultFavoriteComponent
+import com.katorabian.weatherapp.presentation.screen.search.DefaultSearchComponent
+import com.katorabian.weatherapp.presentation.screen.search.OpenReason
+import kotlinx.parcelize.Parcelize
+import javax.inject.Inject
 
-class DefaultRootComponent(
+class DefaultRootComponent @Inject constructor(
+    private val detailsComponentFactory: DefaultDetailsComponent.Factory,
+    private val favoriteComponentFactory: DefaultFavoriteComponent.Factory,
+    private val searchComponentFactory: DefaultSearchComponent.Factory,
     componentContext: ComponentContext
-) : RootComponent, ComponentContext by componentContext
+) : RootComponent, ComponentContext by componentContext {
+
+    override val stack: Value<ChildStack<*, RootComponent.Child>>
+        get() = TODO("Not yet implemented")
+
+    private fun child(
+        config: Config,
+        componentContext: ComponentContext
+    ): RootComponent.Child {
+        return when (config) {
+            is Config.Details -> {
+                val component = detailsComponentFactory.create(
+                    city = config.city,
+                    onBackClick = {},
+                    componentContext = componentContext
+                )
+                RootComponent.Child.Details(component)
+            }
+            Config.Favorite -> {
+                val component = favoriteComponentFactory.create(
+                    onCityItemClick = {},
+                    onAddToFavoriteClick = {},
+                    onSearchClick = {},
+                    componentContext = componentContext
+                )
+                RootComponent.Child.Favorite(component)
+            }
+            is Config.Search -> {
+                val component = searchComponentFactory.create(
+                    openReason = config.openReason,
+                    onBackClick = {},
+                    onSavedToFavorite = {},
+                    onForecastRequest = {},
+                    componentContext = componentContext
+                )
+                RootComponent.Child.Search(component)
+            }
+        }
+    }
+
+    sealed interface Config: Parcelable {
+        @Parcelize
+        data object Favorite: Config
+
+        @Parcelize
+        data class Search(val openReason: OpenReason): Config
+
+        @Parcelize
+        data class Details(val city: City): Config
+    }
+}
