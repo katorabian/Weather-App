3a489703bf8c20d4cc8347ca5084ed64bac0d513
katorabian <katorabian@gmail.com>
2025-06-26 13:14:47 +0700

    feat(component): implement Search- ComponentContext

--- app/src/main/java/com/katorabian/weatherapp/presentation/screen/search/DefaultSearchComponent.kt
+++ app/src/main/java/com/katorabian/weatherapp/presentation/screen/search/DefaultSearchComponent.kt
@@ -1,7 +1,58 @@
 package com.katorabian.weatherapp.presentation.screen.search
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.mvikotlin.core.instancekeeper.getStore
+import com.arkivanov.mvikotlin.extensions.coroutines.labels
+import com.arkivanov.mvikotlin.extensions.coroutines.stateFlow
+import com.katorabian.weatherapp.domain.entity.City
+import com.katorabian.weatherapp.presentation.extensions.lazyComponentScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DefaultSearchComponent(
+class DefaultSearchComponent @Inject constructor(
+    private val openReason: OpenReason,
+    private val searchStoreFactory: SearchStoreFactory,
+    private val onBackClick: () -> Unit,
+    private val onSavedToFavorite: () -> Unit,
+    private val onForecastRequest: (city: City) -> Unit,
     componentContext: ComponentContext
-) : SearchComponent, ComponentContext by componentContext
+) : SearchComponent, ComponentContext by componentContext {
+
+    private val store = instanceKeeper.getStore { searchStoreFactory.create(openReason) }
+    private val scope by lazyComponentScope()
+
+    init {
+        scope.launch {
+            store.labels.onEach {
+                when (it) {
+                    SearchStore.Label.ClickBack -> onBackClick()
+                    is SearchStore.Label.OpenForecast -> onForecastRequest(it.city)
+                    SearchStore.Label.SavedToFavorite -> onSavedToFavorite()
+                }
+            }.collect()
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override val model: StateFlow<SearchStore.State> = store.stateFlow
+
+    override fun changeSearchQuery(query: String) {
+        store.accept(SearchStore.Intent.ChangeSearchQuery(query))
+    }
+
+    override fun onClickBack() {
+        store.accept(SearchStore.Intent.ClickBack)
+    }
+
+    override fun onClickSearch() {
+        store.accept(SearchStore.Intent.ClickSearch)
+    }
+
+    override fun onClickCity(city: City) {
+        store.accept(SearchStore.Intent.ClickCity(city))
+    }
+}
--- app/src/main/java/com/katorabian/weatherapp/presentation/screen/search/SearchComponent.kt
+++ app/src/main/java/com/katorabian/weatherapp/presentation/screen/search/SearchComponent.kt
@@ -1,4 +1,14 @@
 package com.katorabian.weatherapp.presentation.screen.search
 
+import com.katorabian.weatherapp.domain.entity.City
+import kotlinx.coroutines.flow.StateFlow
+
 interface SearchComponent {
+
+    val model: StateFlow<SearchStore.State>
+
+    fun changeSearchQuery(query: String)
+    fun onClickBack()
+    fun onClickSearch()
+    fun onClickCity(city: City)
 }
