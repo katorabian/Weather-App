7ae79c0059c8b5d687bef881ac408f772b206e05
katorabian <katorabian@gmail.com>
2025-06-29 16:57:31 +0700

    feat(content): favorite- screen simple cards

--- app/src/main/java/com/katorabian/weatherapp/presentation/screen/favorite/FavoriteContent.kt
+++ app/src/main/java/com/katorabian/weatherapp/presentation/screen/favorite/FavoriteContent.kt
@@ -1,8 +1,68 @@
 package com.katorabian.weatherapp.presentation.screen.favorite
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FavoriteContent(component: FavoriteComponent) {
 
+    val state by component.model.collectAsState()
+
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxSize(),
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(
+            items = state.cityItems,
+            key = { it.city.id }
+        ) {
+            CityCard(cityItem = it)
+        }
+    }
+}
+
+@Composable
+private fun CityCard(cityItem: FavoriteStore.State.CityItem) {
+    Card(
+        modifier = Modifier.fillMaxSize(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Blue
+        ),
+        shape = MaterialTheme.shapes.extraLarge
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .sizeIn(minHeight = 196.dp)
+                .padding(24.dp),
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.BottomStart),
+                text = cityItem.city.name,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.background
+            )
+        }
+    }
 }
