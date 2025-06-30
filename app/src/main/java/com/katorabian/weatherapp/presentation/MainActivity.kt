2b747b501fff6e6b968ea85a3e3ff0be0aceb443
katorabian <katorabian@gmail.com>
2025-06-30 22:30:40 +0700

    fix(content): disable edgeToEdge

--- app/src/main/java/com/katorabian/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/katorabian/weatherapp/presentation/MainActivity.kt
@@ -3,18 +3,12 @@ package com.katorabian.weatherapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.defaultComponentContext
-import com.katorabian.weatherapp.domain.usecase.ChangeFavoriteStateUseCase
-import com.katorabian.weatherapp.domain.usecase.SearchCityUseCase
 import com.katorabian.weatherapp.presentation.screen.root.DefaultRootComponent
 import com.katorabian.weatherapp.presentation.screen.root.RootContent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +19,6 @@ class MainActivity : ComponentActivity() {
         (applicationContext as WeatherApp).appComponent.inject(this)
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
         setContent {
             Scaffold { paddingValues ->
                 RootContent(
